a54583a2f6807e9b302ea3b38ea5c440733e9c3d
MarcosCoelho <marcos@marcoscoelho.com>
2011-04-16 18:21:40 -0300

    add empty language files for files_attached .. (brazilian and english)

---
+++ system/pyrocms/modules/files/language/brazilian/files_attached_lang.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * PyroCMS
+ *
+ * An open source CMS based on CodeIgniter
+ *
+ * @package		PyroCMS
+ * @author		PyroCMS Dev Team
+ * @license		Apache License v2.0
+ * @link		http://pyrocms.com
+ * @since		Version 1.1.0
+ * @filesource
+ */
+
+$lang['files_attached.attachments_label']					= 'Anexos';
+
+/* End of file files_lang.php */
