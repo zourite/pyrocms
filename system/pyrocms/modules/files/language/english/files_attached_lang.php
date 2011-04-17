02ecf60834e5ab8fe7030b40e7f3394e1b05f156
MarcosCoelho <marcos@marcoscoelho.com>
2011-04-17 14:38:06 -0300

    update files_attached language lines

--- system/pyrocms/modules/files/language/english/files_attached_lang.php
+++ system/pyrocms/modules/files/language/english/files_attached_lang.php
@@ -19,14 +19,24 @@ $lang['files_attached.attachment_type_label']				= 'Origin of the attachment';
 $lang['files_attached.type_file_browser_label']				= 'Files folder';
 $lang['files_attached.type_file_upload_label']				= 'My computer';
 $lang['files_attached.type_link_label']						= 'Web (link)';
-$lang['files_attached.file_browser_label']					= 'Browser file';
-$lang['files_attached.file_upload_label']					= 'Send file';
-$lang['files_attached.link_label']							= 'Insert link';
+$lang['files_attached.do_file_browser_label']				= 'Browser file';
+$lang['files_attached.do_file_upload_label']				= 'Send file';
+$lang['files_attached.do_link_label']						= 'Insert link';
+$lang['files_attached.link_url_label']						= 'URL';
+$lang['files_attached.link_title_label']					= 'Title';
+$lang['files_attached.link_class_label']					= 'Class';
 $lang['files_attached.attach_link_label']					= 'Attach link';
 $lang['files_attached.attachments_key_label']				= 'Internal identification key for Attachments';
 
 // Descriptions
 $lang['files_attached.attachment_type_desc']				= 'Please, select the origin of the attachment to view more options.';
 $lang['files_attached.no_attachments_desc']					= 'No attachments currently.';
+$lang['files_attached.attach_link_desc']					= 'Click here to attach this link';
+
+// Messages
+$lang['files_attached.create_success']						= 'The attachemnt "%s" was created successfully!';
+$lang['files_attached.create_error']						= 'An error occurred while trying to save this attachment.';
+$lang['files_attached.already_exists_error']				= 'The attachment "%s" already exists , do not create duplicate attachments';
+$lang['files_attached.invalid_type_error']					= 'An error occurred, the attachment is unknown or was not indicated correctly.';
 
 /* End of file files_attached_lang.php */
